3dde680a707312ffae660a1c497a34837942ca05
dinh-huong-9 <90088145+dinh-huong-9@users.noreply.github.com>
2023-04-20 15:13:25 +0700

    xongtrangchitiet

--- Nhom18_TTCM_CNTTVA1_K61/Controllers/HomeController.cs
+++ Nhom18_TTCM_CNTTVA1_K61/Controllers/HomeController.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Nhom18_TTCM_CNTTVA1_K61.Models;
 namespace Nhom18_TTCM_CNTTVA1_K61.Controllers
 {
     public class HomeController : Controller
     {
+        QLtrasenEntities db = new QLtrasenEntities();
         public ActionResult Index()
         {
             return View();
@@ -15,8 +16,9 @@ namespace Nhom18_TTCM_CNTTVA1_K61.Controllers
 
         public ActionResult About()
         {
-          
-            return View();
+            List<News> bao = db.News.ToList();
+            return PartialView(bao);
+           
         }
 
         public ActionResult Contact()
@@ -31,8 +33,28 @@ namespace Nhom18_TTCM_CNTTVA1_K61.Controllers
         }
         public ActionResult Product()
         {
-
-            return View();
+            List<Product> sp = db.Products.ToList();
+            return PartialView(sp);
+        }
+        public ActionResult Detail(string Idproduct)
+        {
+            var chitiet = db.Products.SingleOrDefault(n => n.Idproduct == Idproduct);
+            if (chitiet == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(chitiet);
+        }
+        public ActionResult DetailNews(string IDcontent)
+        {
+            var dcontent = db.News.SingleOrDefault(n => n.IDcontent == IDcontent);
+            if (dcontent == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(dcontent);
         }
     }
 }
---
+++ Nhom18_TTCM_CNTTVA1_K61/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Nhom18_TTCM_CNTTVA1_K61.Models;
+namespace Nhom18_TTCM_CNTTVA1_K61.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        QLtrasenEntities db = new QLtrasenEntities();
+        
+        // ĐĂNG KÝ
+      
+
+        // ĐĂNG KÝ PHƯƠNG THỨC POST
+        [HttpPost]
+
+        public ActionResult Dangnhap()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult Dangnhap(FormCollection userlog)
+        {
+            string userMail = userlog["userMail"].ToString();
+            string password = userlog["password"].ToString();
+            var islogin = db.Users.SingleOrDefault(x => x.Email.Equals(userMail) && x.Pass.Equals(password));
+
+            if (islogin != null)
+            {
+                if (userMail == "Admin@gmail.com")
+                {
+                    Session["use"] = islogin;
+                    return RedirectToAction("Index", "Admin/Home");
+                }
+                else
+                {
+                    Session["use"] = islogin;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                ViewBag.Fail = "Đăng nhập thất bại";
+                return View("Dangnhap");
+            }
+
+        }
+        public ActionResult DangXuat()
+        {
+            Session["use"] = null;
+            return RedirectToAction("Index", "Home");
+
+        }
+    }
+}
